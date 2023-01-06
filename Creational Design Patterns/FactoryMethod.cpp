dc1e2d262fec0ab819196db73dfe8572295fc920
Frozen <93019192+AlterFrozen@users.noreply.github.com>
2023-01-06 21:29:12 +0800

    Factory Method

--- Creational Design Patterns/FactoryMethod.cpp
+++ Creational Design Patterns/FactoryMethod.cpp
@@ -1,3 +1,9 @@
+/*[Summary]
+Factory Method is a creational design pattern that provides 
+an interface for creating objects in a superclass, but allows 
+subclasses to alter the type of objects that will be created.
+*/
+
 #include <iostream>
 #include <vector>
 #include <memory>
